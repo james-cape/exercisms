63d3c70538428ab1071d2c684a2500b23da21796
james.cape <james.cape@gmail.com>
2019-10-15 09:58:40 -0600

    Adds more descriptive variable names

--- python/markdown/markdown.py
+++ python/markdown/markdown.py
@@ -1,25 +1,25 @@
 import re
 
-
 def parse(markdown):
     lines = markdown.split('\n')
-    res = ''
+    accumulator = ''
     in_list = False
     in_list_append = False
-    for i in lines:
-        if re.match('###### (.*)', i) is not None:
-            i = '<h6>' + i[7:] + '</h6>'
-        elif re.match('## (.*)', i) is not None:
-            i = '<h2>' + i[3:] + '</h2>'
-        elif re.match('# (.*)', i) is not None:
-            i = '<h1>' + i[2:] + '</h1>'
-        m = re.match(r'\* (.*)', i)
-        if m:
+    for line in lines:
+        if re.match('###### (.*)', line) is not None:
+            line = '<h6>' + line[7:] + '</h6>'
+        elif re.match('## (.*)', line) is not None:
+            line = '<h2>' + line[3:] + '</h2>'
+        elif re.match('# (.*)', line) is not None:
+            line = '<h1>' + line[2:] + '</h1>'
+        match_object = re.match(r'\* (.*)', line)
+        # breakpoint()
+        if match_object:
             if not in_list:
                 in_list = True
                 is_bold = False
                 is_italic = False
-                curr = m.group(1)
+                curr = match_object.group(1)
                 m1 = re.match('(.*)__(.*)__(.*)', curr)
                 if m1:
                     curr = m1.group(1) + '<strong>' + \
@@ -30,11 +30,11 @@ def parse(markdown):
                     curr = m1.group(1) + '<em>' + m1.group(2) + \
                         '</em>' + m1.group(3)
                     is_italic = True
-                i = '<ul><li>' + curr + '</li>'
+                line = '<ul><li>' + curr + '</li>'
             else:
                 is_bold = False
                 is_italic = False
-                curr = m.group(1)
+                curr = match_object.group(1)
                 m1 = re.match('(.*)__(.*)__(.*)', curr)
                 if m1:
                     is_bold = True
@@ -47,25 +47,25 @@ def parse(markdown):
                 if is_italic:
                     curr = m1.group(1) + '<em>' + m1.group(2) + \
                         '</em>' + m1.group(3)
-                i = '<li>' + curr + '</li>'
+                line = '<li>' + curr + '</li>'
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
 
-        m = re.match('<h|<ul|<p|<li', i)
-        if not m:
-            i = '<p>' + i + '</p>'
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + m.group(2) + '</strong>' + m.group(3)
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + m.group(2) + '</em>' + m.group(3)
+        match_object = re.match('<h|<ul|<p|<li', line)
+        if not match_object:
+            line = '<p>' + line + '</p>'
+        match_object = re.match('(.*)__(.*)__(.*)', line)
+        if match_object:
+            line = match_object.group(1) + '<strong>' + match_object.group(2) + '</strong>' + match_object.group(3)
+        match_object = re.match('(.*)_(.*)_(.*)', line)
+        if match_object:
+            line = match_object.group(1) + '<em>' + match_object.group(2) + '</em>' + match_object.group(3)
         if in_list_append:
-            i = '</ul>' + i
+            line = '</ul>' + line
             in_list_append = False
-        res += i
+        accumulator += line
     if in_list:
-        res += '</ul>'
-    return res
+        accumulator += '</ul>'
+    return accumulator
