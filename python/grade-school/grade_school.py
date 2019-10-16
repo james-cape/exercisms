0cd15b12b53ca889807648c2624496e01f411c3f
james.cape <james.cape@gmail.com>
2019-10-16 15:38:14 -0600

    Completes grade_school exercism

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,24 @@
 class School(object):
     def __init__(self):
-        pass
+        self.student_info = []
 
     def add_student(self, name, grade):
-        pass
+        student = Student(name, grade)
+        self.student_info.append(student)
 
     def roster(self):
-        pass
+        return [student.name for student in self._list_by_name_and_grade()]
 
     def grade(self, grade_number):
-        pass
+        return [student.name for student in self._limit_by_grade(grade_number)]
+
+    def _list_by_name_and_grade(self):
+        return sorted(self.student_info, key=lambda x: (x.grade, x.name))
+
+    def _limit_by_grade(self, grade_number):
+        return list(filter(lambda x: x.grade == grade_number, self._list_by_name_and_grade()))
+
+class Student():
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
