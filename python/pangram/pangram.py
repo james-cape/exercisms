8f5a018eb707481ae142cee19c9588e69856d0d7
james.cape <james.cape@gmail.com>
2019-10-17 12:36:06 -0600

    Adds solution for pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,8 @@
 def is_pangram(sentence):
-    pass
+
+    alphabet = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+        'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+    return all(letter in sentence.lower() for letter in alphabet)
