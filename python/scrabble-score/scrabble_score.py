ea96d57bf7e247ded172089dfec499a39e1a99da
james.cape <james.cape@gmail.com>
2019-10-17 08:36:36 -0600

    Initial acronym commit

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,18 @@
 def score(word):
-    pass
+    score = 0
+    for letter in word:
+        if letter.lower() in "aeioulnrst":
+            score += 1
+        elif letter.lower() in "dg":
+            score += 2
+        elif letter.lower() in "bcmp":
+            score += 3
+        elif letter.lower() in "fhvwy":
+            score += 4
+        elif letter.lower() in "k":
+            score += 5
+        elif letter.lower() in "jx":
+            score += 8
+        elif letter.lower() in "qz":
+            score += 10
+    return score
