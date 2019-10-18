49de3d2760a77429230a837693b584b9eb74a54f
james.cape <james.cape@gmail.com>
2019-10-18 09:51:01 -0600

    Adds solution for hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("String lengths don't match")
+    counter = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] is not strand_b[i]:
+            counter += 1
+
+    return counter
