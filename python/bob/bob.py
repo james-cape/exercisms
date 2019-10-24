8eec2f62ad2c4df65c425ac0f82a703d13fc1f67
james.cape <james.cape@gmail.com>
2019-10-24 11:06:54 -0600

    Adds solution for bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,15 @@
 def response(hey_bob):
-    pass
+    for filler in [' ', '\t', '\r', '\n']:
+        hey_bob = hey_bob.replace(filler, '')
+    has_alpha = any([n.isalpha() for n in hey_bob])
+    if not hey_bob:
+        response = 'Fine. Be that way!'
+    elif hey_bob.upper() == hey_bob and hey_bob.strip()[-1] == '?' and has_alpha:
+        response = "Calm down, I know what I'm doing!"
+    elif hey_bob.upper() == hey_bob and has_alpha:
+        response = "Whoa, chill out!"
+    elif hey_bob.strip()[-1] == '?':
+        response = 'Sure.'
+    else:
+        response = "Whatever."
+    return response
