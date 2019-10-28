8c85443c49c744426d557a0bb1bda159e4adc49a
james.cape <james.cape@gmail.com>
2019-10-28 15:19:26 -0600

    Refactors solution for run_length

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -3,38 +3,33 @@ def decode(string):
     decoded = ''
     last_character = ''
     for character in string:
-        if character:
-            if not character.isnumeric():
-                if last_character.isnumeric():
-                    if occurences:
-                        decoded += character * int(occurences)
-                    else:
-                        decoded += character
-                    occurences = ''
-                else:
-                    decoded += character
+        if character and character.isnumeric():
+            occurences += character
+        elif last_character.isnumeric():
+            if occurences:
+                decoded += character * int(occurences)
             else:
-                occurences += character
+                decoded += character
+            occurences = ''
+        else:
+            decoded += character
         last_character = character
     return decoded
-
 
 def encode(string):
     counter = 1
     last_character = ''
     encoded = ''
     for character in string:
-        if last_character:
-            if character == last_character:
-                counter += 1
-            else:
-                if counter != 1:
-                    encoded += str(counter)
-                encoded += last_character
-                counter = 1
-        last_character = character
-    if string:
-        if counter != 1:
-            encoded += str(counter)
-        encoded += last_character
+        if last_character and character == last_character:
+            counter += 1
+        else:
+            if counter != 1:
+                encoded += str(counter)
+            encoded += last_character
+            counter = 1
+            last_character = character
+    if string and counter != 1:
+        encoded += str(counter)
+    encoded += last_character
     return encoded
