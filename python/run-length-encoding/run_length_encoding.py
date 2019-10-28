23308d360851ff474daf903c1f5f611c853214ee
james.cape <james.cape@gmail.com>
2019-10-28 11:11:20 -0600

    Adds solution for run_length

--- python/run-length-encoding/run_length_encoding.py
+++ python/run-length-encoding/run_length_encoding.py
@@ -1,6 +1,40 @@
 def decode(string):
-    pass
+    occurences = ''
+    decoded = ''
+    last_character = ''
+    for character in string:
+        if character:
+            if not character.isnumeric():
+                if last_character.isnumeric():
+                    if occurences:
+                        decoded += character * int(occurences)
+                    else:
+                        decoded += character
+                    occurences = ''
+                else:
+                    decoded += character
+            else:
+                occurences += character
+        last_character = character
+    return decoded
 
 
 def encode(string):
-    pass
+    counter = 1
+    last_character = ''
+    encoded = ''
+    for character in string:
+        if last_character:
+            if character == last_character:
+                counter += 1
+            else:
+                if counter != 1:
+                    encoded += str(counter)
+                encoded += last_character
+                counter = 1
+        last_character = character
+    if string:
+        if counter != 1:
+            encoded += str(counter)
+        encoded += last_character
+    return encoded
