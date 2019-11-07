57e029a2acca5ea7fc65aa89a4b1bcf276b9b4f4
james.cape <james.cape@gmail.com>
2019-11-07 14:52:13 -0700

    Refactors ranges into a dictionary

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -3,40 +3,24 @@ import calendar
 
 def meetup(year, month, week, day_of_week):
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    if week == 'teenth':
-        for i in range(13, 20):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == '1st':
-        for i in range(1, 8):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == '2nd':
-        for i in range(8, 15):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == '3rd':
-        for i in range(15, 22):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == '4th':
-        for i in range(22, 29):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == 'last':
-        # breakpoint()
-        days_in_month = calendar.monthrange(year, month)[1]
-        for i in range(days_in_month - 6, days_in_month + 1):
-            if days[calendar.weekday(year, month, i)] == day_of_week:
-                return date(year, month, i)
-    if week == '5th':
-        if month == 2:
-            raise MeetupDayException('Only four weeks in Feb')
-        else:
-            for i in range(29, calendar.monthrange(year, month)[1] + 1):
-                if days[calendar.weekday(year, month, i)] == day_of_week:
-                    return date(year, month, i)
-
+    days_in_month = calendar.monthrange(year, month)[1]
+    ranges = {
+        'teenth': range(13, 20),
+        '1st': range(1, 8),
+        '2nd': range(8, 15),
+        '3rd': range(15, 22),
+        '4th': range(22, 29),
+        '5th': range(29, calendar.monthrange(year, month)[1] + 1),
+        'last': range(days_in_month - 6, days_in_month + 1)
+    }
+    
+    if month == 2 and week == '5th':
+        raise MeetupDayException('Only four weeks in Feb')
+    
+    for i in ranges[week]:
+        if days[calendar.weekday(year, month, i)] == day_of_week:
+            return date(year, month, i)
+    
 class MeetupDayException(Exception):
     def __init__(self, error_message):
         self.error_message = error_message
