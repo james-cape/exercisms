f7dfdd15366cd93ce1f5939c069d22bdcf8be070
james.cape <james.cape@gmail.com>
2019-11-07 14:58:19 -0700

    Changes i to day for readability

--- python/meetup/meetup.py
+++ python/meetup/meetup.py
@@ -13,13 +13,11 @@ def meetup(year, month, week, day_of_week):
         '5th': range(29, calendar.monthrange(year, month)[1] + 1),
         'last': range(days_in_month - 6, days_in_month + 1)
     }
-    
-    if month == 2 and week == '5th':
+    if month == 2 and week == '5th': 
         raise MeetupDayException('Only four weeks in Feb')
-    
-    for i in ranges[week]:
-        if days[calendar.weekday(year, month, i)] == day_of_week:
-            return date(year, month, i)
+    for day in ranges[week]:
+        if days[calendar.weekday(year, month, day)] == day_of_week:
+            return date(year, month, day)
     
 class MeetupDayException(Exception):
     def __init__(self, error_message):
