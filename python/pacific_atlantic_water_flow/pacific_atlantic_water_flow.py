978ac19d239fed927ec37f1a8e97da48a92b35f7
james.cape <james.cape@gmail.com>
2020-06-01 08:30:31 -0600

    Adds leetcode solution

--- python/pacific_atlantic_water_flow/pacific_atlantic_water_flow.py
+++ python/pacific_atlantic_water_flow/pacific_atlantic_water_flow.py
@@ -1,44 +1,44 @@
-class Solution:
-    def get_divide(self, matrix):
-        if not matrix:
-            return []
-        # Set initial hashes of pacific and atlantic:
-        number_rows = len(matrix)
-        number_columns = len(matrix[0])
-        pacific = {'name': 'pacific'}
-        atlantic = {'name': 'atlantic'}
-        revisit = []
+# class Solution:
+#     def get_divide(self, matrix):
+#         if not matrix:
+#             return []
+#         # Set initial hashes of pacific and atlantic:
+#         number_rows = len(matrix)
+#         number_columns = len(matrix[0])
+#         pacific = {'name': 'pacific'}
+#         atlantic = {'name': 'atlantic'}
+#         revisit = []
 
-        self.set_initial_ocean_coordinates(matrix, pacific, atlantic)
-
-
-        # Starting with pacific corner
-        for x in range(number_rows):
-            for y in range(number_columns):
-                self.assign_ocean_if_connected(x, y, pacific, revisit, matrix)
+#         self.set_initial_ocean_coordinates(matrix, pacific, atlantic)
 
 
-        # Starting with atlantic corner
-        for x in range(number_rows-1, -1, -1):
-            for y in range(number_columns-1, -1, -1):
-                # Check for pacific connection
-                self.assign_ocean_if_connected(x, y, atlantic, revisit, matrix)
+#         # Starting with pacific corner
+#         for x in range(number_rows):
+#             for y in range(number_columns):
+#                 self.assign_ocean_if_connected(x, y, pacific, revisit, matrix)
 
-        return self.find_divide(pacific, atlantic)
 
-    def assign_ocean_if_connected(self, x, y, ocean, revisit, matrix):
-        if f'{x}, {y}' not in ocean:
-            number_rows = len(matrix)
-            number_columns = len(matrix[0])
-            # If so, add x,y to ocean hash (because it can flow down)
-            # Checks for any neighbor 1) on board, 2) flows to ocean, and 3) lower or equal to x,y elevation
-            if (
-                x+1 in range(number_rows) and f'{x+1}, {y}' in ocean and matrix[x][y] >= matrix[x+1][y] or
-                x-1 in range(number_rows) and f'{x-1}, {y}' in ocean and matrix[x][y] >= matrix[x-1][y] or
-                y+1 in range(number_columns) and f'{x}, {y+1}' in ocean and matrix[x][y] >= matrix[x][y+1] or
-                y-1 in range(number_columns) and f'{x}, {y-1}' in ocean and matrix[x][y] >= matrix[x][y-1]
-            ):
-                ocean[f'{x}, {y}'] = matrix[x][y]
+#         # Starting with atlantic corner
+#         for x in range(number_rows-1, -1, -1):
+#             for y in range(number_columns-1, -1, -1):
+#                 # Check for pacific connection
+#                 self.assign_ocean_if_connected(x, y, atlantic, revisit, matrix)
+
+#         return self.find_divide(pacific, atlantic)
+
+#     def assign_ocean_if_connected(self, x, y, ocean, revisit, matrix):
+#         if f'{x}, {y}' not in ocean:
+#             number_rows = len(matrix)
+#             number_columns = len(matrix[0])
+#             # If so, add x,y to ocean hash (because it can flow down)
+#             # Checks for any neighbor 1) on board, 2) flows to ocean, and 3) lower or equal to x,y elevation
+#             if (
+#                 x+1 in range(number_rows) and f'{x+1}, {y}' in ocean and matrix[x][y] >= matrix[x+1][y] or
+#                 x-1 in range(number_rows) and f'{x-1}, {y}' in ocean and matrix[x][y] >= matrix[x-1][y] or
+#                 y+1 in range(number_columns) and f'{x}, {y+1}' in ocean and matrix[x][y] >= matrix[x][y+1] or
+#                 y-1 in range(number_columns) and f'{x}, {y-1}' in ocean and matrix[x][y] >= matrix[x][y-1]
+#             ):
+#                 ocean[f'{x}, {y}'] = matrix[x][y]
 
 
 
@@ -60,6 +60,81 @@ class Solution:
 
 
 
+    # def set_initial_ocean_coordinates(self, matrix, pacific, atlantic):
+    #     number_rows = len(matrix)
+    #     number_columns = len(matrix[0])
+
+    #     for y in range(number_columns):
+    #         # Top row
+    #         pacific[f'0, {y}'] = matrix[0][y]
+    #         # Bottom row
+    #         atlantic[f'{number_rows-1}, {y}'] = matrix[number_rows-1][y]
+        
+    #     for x in range(number_rows):
+    #         # Left column
+    #         pacific[f'{x}, 0'] = matrix[x][0]
+    #         # Right column
+    #         atlantic[f'{x}, {number_columns-1}'] = matrix[x][number_columns-1]
+
+    # def find_divide(self, pacific, atlantic):
+    #     continental_divide = []
+    #     for key in list(pacific.keys()):
+    #         if key != 'name' and key in atlantic:
+    #             continental_divide.append([int(x) for x in key.split(', ')])
+    #     return continental_divide
+
+
+
+
+
+class Solution:
+    def get_divide(self, matrix):
+        if not matrix:
+            return []
+        # Set initial hashes of pacific and atlantic:
+        number_rows = len(matrix)
+        number_columns = len(matrix[0])
+        pacific = {}
+        atlantic = {}
+
+        self.set_initial_ocean_coordinates(matrix, pacific, atlantic)
+
+        all_mapped = False
+        while not all_mapped:
+            all_mapped = True
+            # Starting with pacific corner
+            for x in range(number_rows):
+                for y in range(number_columns):
+                    if f'{x}, {y}' not in pacific:
+                        # Check if elevation is higher or equal to any n/s/e/w neighbors in pacific hash
+                        # If so, add x,y to pacific hash (because it can flow down)
+                        if self.flows_lower(x, y, matrix, ocean=pacific):
+                            all_mapped = False
+                            pacific[f'{x}, {y}'] = matrix[x][y]
+
+            # Starting with atlantic corner
+            for x in range(number_rows-1, -1, -1):
+                for y in range(number_columns-1, -1, -1):
+                    # Check for pacific connection
+                    if f'{x}, {y}' not in atlantic:
+                        # Check if elevation is higher or equal to any n/s/e/w neighbors in atlantic hash
+                        # If so, add x,y to atlantic hash (because it can flow down)
+                        if self.flows_lower(x, y, matrix, ocean=atlantic):
+                            all_mapped = False
+                            atlantic[f'{x}, {y}'] = matrix[x][y]
+
+        return self.find_divide(pacific, atlantic)
+
+
+    def flows_lower(self, x, y, matrix, ocean):
+        # Checks for any neighbor 1) on board, 2) flows to ocean, and 3) lower or equal to x,y elevation
+        return (
+            x+1 in range(len(matrix)) and f'{x+1}, {y}' in ocean and matrix[x][y] >= matrix[x+1][y] or
+            x-1 in range(len(matrix)) and f'{x-1}, {y}' in ocean and matrix[x][y] >= matrix[x-1][y] or
+            y+1 in range(len(matrix[0])) and f'{x}, {y+1}' in ocean and matrix[x][y] >= matrix[x][y+1] or
+            y-1 in range(len(matrix[0])) and f'{x}, {y-1}' in ocean and matrix[x][y] >= matrix[x][y-1]
+        )
+
     def set_initial_ocean_coordinates(self, matrix, pacific, atlantic):
         number_rows = len(matrix)
         number_columns = len(matrix[0])
@@ -79,57 +154,83 @@ class Solution:
     def find_divide(self, pacific, atlantic):
         continental_divide = []
         for key in list(pacific.keys()):
-            if key != 'name' and key in atlantic:
+            if key in atlantic:
                 continental_divide.append([int(x) for x in key.split(', ')])
         return continental_divide
 
 
-#     matrix = [
-#         [ 1, 2, 2, 3,*5],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
-#     matrix = [
-#         [ P, 2, 2, 3,*5],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
-#     matrix = [
-#         [ P, P, 2, 3,*5],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
-#     matrix = [
-#         [ P, P, P, 3,*5],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
-#     matrix = [
-#         [ P, P, P, P,PA],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
-# ----
-#     matrix = [
-#         [ 1, 2, 2, 3,*5],
-#         [ 3, 2, 3,*4,*4],
-#         [ 2, 4,*5, 3, 1],
-#         [*6,*7, 1, 4, 5],
-#         [*5, 1, 1, 2, 4]
-#     ]
-# ----
+
+
+
+
+
+# class Solution:
+#     def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
+#         if not matrix:
+#             return []
+#         # Set initial hashes of pacific and atlantic:
+#         number_rows = len(matrix)
+#         number_columns = len(matrix[0])
+#         pacific = {}
+#         atlantic = {}
+
+#         self.set_initial_ocean_coordinates(matrix, pacific, atlantic)
+
+#         all_mapped = False
+#         while not all_mapped:
+#             all_mapped = True
+#             # Starting with pacific corner
+#             for x in range(number_rows):
+#                 for y in range(number_columns):
+#                     if f'{x}, {y}' not in pacific:
+#                         # Check if elevation is higher or equal to any n/s/e/w neighbors in pacific hash
+#                         # If so, add x,y to pacific hash (because it can flow down)
+#                         if self.flows_lower(x, y, matrix, ocean=pacific):
+#                             all_mapped = False
+#                             pacific[f'{x}, {y}'] = matrix[x][y]
+
+#             # Starting with atlantic corner
+#             for x in range(number_rows-1, -1, -1):
+#                 for y in range(number_columns-1, -1, -1):
+#                     # Check for pacific connection
+#                     if f'{x}, {y}' not in atlantic:
+#                         # Check if elevation is higher or equal to any n/s/e/w neighbors in atlantic hash
+#                         # If so, add x,y to atlantic hash (because it can flow down)
+#                         if self.flows_lower(x, y, matrix, ocean=atlantic):
+#                             all_mapped = False
+#                             atlantic[f'{x}, {y}'] = matrix[x][y]
+
+#         return self.find_divide(pacific, atlantic)
+
+
+#     def flows_lower(self, x, y, matrix, ocean):
+#         # Checks for any neighbor 1) on board, 2) flows to ocean, and 3) lower or equal to x,y elevation
+#         return (
+#             x+1 in range(len(matrix)) and f'{x+1}, {y}' in ocean and matrix[x][y] >= ocean[f'{x+1}, {y}'] or
+#             x-1 in range(len(matrix)) and f'{x-1}, {y}' in ocean and matrix[x][y] >= ocean[f'{x-1}, {y}'] or
+#             y+1 in range(len(matrix[0])) and f'{x}, {y+1}' in ocean and matrix[x][y] >= ocean[f'{x}, {y+1}'] or
+#             y-1 in range(len(matrix[0])) and f'{x}, {y-1}' in ocean and matrix[x][y] >= ocean[f'{x}, {y-1}']
+#         )
+
+#     def set_initial_ocean_coordinates(self, matrix, pacific, atlantic):
+#         number_rows = len(matrix)
+#         number_columns = len(matrix[0])
+
+#         for y in range(number_columns):
+#             # Top row
+#             pacific[f'0, {y}'] = matrix[0][y]
+#             # Bottom row
+#             atlantic[f'{number_rows-1}, {y}'] = matrix[number_rows-1][y]
+        
+#         for x in range(number_rows):
+#             # Left column
+#             pacific[f'{x}, 0'] = matrix[x][0]
+#             # Right column
+#             atlantic[f'{x}, {number_columns-1}'] = matrix[x][number_columns-1]
+
+#     def find_divide(self, pacific, atlantic):
+#         continental_divide = []
+#         for key in list(pacific.keys()):
+#             if key in atlantic:
+#                 continental_divide.append([int(x) for x in key.split(', ')])
+#         return continental_divide
--- python/pacific_atlantic_water_flow/test_pacific_atlantic_water_flow.py
+++ python/pacific_atlantic_water_flow/test_pacific_atlantic_water_flow.py
@@ -13,6 +13,13 @@ def test_case_1():
         [6,7,1,4,5],
         [5,1,1,2,4]
     ]
+    # grid = [
+    #     [ 1,*2, 2,*3, 5],
+    #     [ 3, 2, 3, 4, 4],
+    #     [ 2, 4, 5, 3, 1],
+    #     [ 6, 7, 1,*4,*5],
+    #     [ 5, 1,*1,*2,*4]
+    # ]
     answer = [
         [0,4],
         [1,3],
@@ -32,27 +39,27 @@ def test_case_2():
     answer = []
     assert solution.get_divide(grid) == []
 
-# def test_case_3():
-#     solution = Solution()
-#     grid = [
-#         [1,2,3],
-#         [8,9,4],
-#         [7,6,5]
-#     ]
-#     grid = [
-#         [1,2,7,7,2],
-#         [8,9,6,6,2],
-#         [7,6,5,2,2]
-#     ]
-#     answer = [
-#         [0,2],
-#         [1,0],
-#         [1,1],
-#         [1,2],
-#         [2,0],
-#         [2,1], #
-#         [2,2]
-#     ]
-#     for coordinate in solution.get_divide(grid):
-#         assert coordinate in answer
-#     assert len(answer) == len(solution.get_divide(grid))
+def test_case_3():
+    solution = Solution()
+    grid = [
+        [1,2,3],
+        [8,9,4],
+        [7,6,5]
+    ]
+    # grid = [
+    #     [1,2,7,7,2],
+    #     [8,9,6,6,2],
+    #     [7,6,5,2,2]
+    # ]
+    answer = [
+        [0,2],
+        [1,0],
+        [1,1],
+        [1,2],
+        [2,0],
+        [2,1], #
+        [2,2]
+    ]
+    for coordinate in solution.get_divide(grid):
+        assert coordinate in answer
+    assert len(answer) == len(solution.get_divide(grid))
