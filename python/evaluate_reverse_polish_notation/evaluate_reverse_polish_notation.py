884bbd6207792c9659937f10e0472360b732631d
james.cape <james.cape@gmail.com>
2020-06-07 14:16:44 -0600

    Adds initial solution

--- python/evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.py
+++ python/evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.py
@@ -1,3 +1,52 @@
 class Solution:
     def get_answer(self, tokens):
-        pass
+        operators = ['+', '-', '*', '/']
+        index = 2
+
+        while len(tokens) >= 3:
+            if tokens[index] in operators:
+                new_element = self.calculate(tokens[index-2], tokens[index-1], tokens[index])
+                if index >= 3:
+                    tokens = tokens[:index-2] + [new_element] + tokens[index+1:]
+                else:
+                    tokens = [new_element] + tokens[index+1:]
+                index = index - 2
+            index += 1
+        return int(round(float(tokens[0]), 0))
+
+    def calculate(self, operand_1, operand_2, operator):
+        if operator == '+':
+            return str(float(operand_1) + float(operand_2))
+        elif operator == '-':
+            return str(float(operand_1) - float(operand_2))
+        elif operator == '*':
+            return str(float(operand_1) * float(operand_2))
+        elif operator == '/':
+            return str(int(float(operand_1) / float(operand_2)))
+
+
+# class Solution:
+#     def get_answer(self, tokens):
+#         operators = ['+', '-', '*', '/']
+#         index = 2
+
+#         while len(tokens) >= 3:
+#             if tokens[index] in operators:
+#                 new_element = self.calculate(tokens[index-2], tokens[index-1], tokens[index])
+#                 if index >= 3:
+#                     tokens = tokens[:index-2] + [new_element] + tokens[index+1:]
+#                 else:
+#                     tokens = [new_element] + tokens[index+1:]
+#                 index = index - 2
+#             index += 1
+#         return int(round(float(tokens[0]), 0))
+
+#     def calculate(self, operand_1, operand_2, operator):
+#         if operator == '+':
+#             return str(float(operand_1) + float(operand_2))
+#         elif operator == '-':
+#             return str(float(operand_1) - float(operand_2))
+#         elif operator == '*':
+#             return str(float(operand_1) * float(operand_2))
+#         elif operator == '/':
+#             return str(int(float(operand_1) / float(operand_2)))
--- python/evaluate_reverse_polish_notation/test_evaluate_reverse_polish_notation.py
+++ python/evaluate_reverse_polish_notation/test_evaluate_reverse_polish_notation.py
@@ -3,3 +3,34 @@ from evaluate_reverse_polish_notation import Solution
 def test_it_initializes():
     solution = Solution()
     assert solution
+
+def test_case_1():
+    solution = Solution()
+    tokens = ["2", "1", "+", "3", "*"]
+    answer = 9
+    # Explanation: ((2 + 1) * 3) = 9
+
+    assert solution.get_answer(tokens) == answer
+
+def test_case_2():
+    solution = Solution()
+    tokens = ["4", "13", "5", "/", "+"]
+    answer = 6
+    # Explanation: (4 + (13 / 5)) = 6
+
+    assert solution.get_answer(tokens) == answer
+
+def test_case_3():
+    solution = Solution()
+    tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+    answer = 22
+    # Explanation: 
+    #   ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+    # = ((10 * (6 / (12 * -11))) + 17) + 5
+    # = ((10 * (6 / -132)) + 17) + 5
+    # = ((10 * 0) + 17) + 5
+    # = (0 + 17) + 5
+    # = 17 + 5
+    # = 22
+
+    assert solution.get_answer(tokens) == answer
