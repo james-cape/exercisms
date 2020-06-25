b3b8b15300eb85035e2b13abc1986a23301943b4
james.cape <james.cape@gmail.com>
2020-06-25 16:45:24 -0600

    Rewrites to use a stack instead of array slice

--- python/evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.py
+++ python/evaluate_reverse_polish_notation/evaluate_reverse_polish_notation.py
@@ -1,52 +1,39 @@
 class Solution:
     def get_answer(self, tokens):
-        operators = ['+', '-', '*', '/']
-        index = 2
+        operators = {
+            '+': self.add_solution,
+            '-': self.sub_solution,
+            '*': self.mul_solution,
+            '/': self.div_solution
+        }
+        results_stack = []  # Stack
 
-        while len(tokens) >= 3:
-            if tokens[index] in operators:
-                new_element = self.calculate(tokens[index-2], tokens[index-1], tokens[index])
-                if index >= 3:
-                    tokens = tokens[:index-2] + [new_element] + tokens[index+1:]
-                else:
-                    tokens = [new_element] + tokens[index+1:]
-                index = index - 2
-            index += 1
-        return int(round(float(tokens[0]), 0))
+        for token in tokens:
+            if token not in operators:
+                results_stack.append(token)
+            else:
+                last_element = results_stack.pop()
+                second_to_last_element = results_stack.pop()
+                result = operators[token](second_to_last_element, last_element)
+                results_stack.append(result)
+        return int(round(float(results_stack[0]), 0))
 
-    def calculate(self, operand_1, operand_2, operator):
-        if operator == '+':
-            return str(float(operand_1) + float(operand_2))
-        elif operator == '-':
-            return str(float(operand_1) - float(operand_2))
-        elif operator == '*':
-            return str(float(operand_1) * float(operand_2))
-        elif operator == '/':
-            return str(int(float(operand_1) / float(operand_2)))
+    def add_solution(self, operand_1, operand_2):
+        return str(float(operand_1) + float(operand_2))
+
+    def sub_solution(self, operand_1, operand_2):
+        return str(float(operand_1) - float(operand_2))
+
+    def mul_solution(self, operand_1, operand_2):
+        return str(float(operand_1) * float(operand_2))
+
+    def div_solution(self, operand_1, operand_2):
+        return str(int(float(operand_1) / float(operand_2)))
 
 
-# class Solution:
-#     def get_answer(self, tokens):
-#         operators = ['+', '-', '*', '/']
-#         index = 2
 
-#         while len(tokens) >= 3:
-#             if tokens[index] in operators:
-#                 new_element = self.calculate(tokens[index-2], tokens[index-1], tokens[index])
-#                 if index >= 3:
-#                     tokens = tokens[:index-2] + [new_element] + tokens[index+1:]
-#                 else:
-#                     tokens = [new_element] + tokens[index+1:]
-#                 index = index - 2
-#             index += 1
-#         return int(round(float(tokens[0]), 0))
-
-#     def calculate(self, operand_1, operand_2, operator):
-#         if operator == '+':
-#             return str(float(operand_1) + float(operand_2))
-#         elif operator == '-':
-#             return str(float(operand_1) - float(operand_2))
-#         elif operator == '*':
-#             return str(float(operand_1) * float(operand_2))
-#         elif operator == '/':
-#             return str(int(float(operand_1) / float(operand_2)))
+#   tokens = ["2", "1", "+", "3", "*"]
+#   2 [2]
+#   1 [2, 1]
+#   + [3]
+#   3 [9]
