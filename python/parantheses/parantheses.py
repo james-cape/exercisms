b1e0d2dd7f5503e3302b46b70b4eca700e733926
james.cape <james.cape@gmail.com>
2020-08-31 07:14:17 -0600

    parantheses

--- python/parantheses/parantheses.py
+++ python/parantheses/parantheses.py
@@ -1,41 +1,33 @@
 class Parantheses:
-    def __init__(self):
-        pass
-
     def generateParanthesis(self, n):
         results = []
         differential = 0  # closings - openings
         opening_parens = n
         closing_parens = n
-        
         combo = ''
 
-        def add_opening(results, differential, opening_parens, closing_parens, combo):
-            if opening_parens > 0 and differential >= 0:
-                combo += '('
-                opening_parens -= 1
-                differential += 1
-                if len(combo) < n*2:
-                    add_opening(results, differential, opening_parens, closing_parens, combo)
-                    add_closing(results, differential, opening_parens, closing_parens, combo)
-                else:
-                    results.append(combo)
+        def add_char(results, differential, opening_parens, closing_parens, combo, char):
+            if differential >=0:
+                if char == '(' and opening_parens > 0:
+                    combo += char
+                    opening_parens -= 1
+                    differential += 1
+                    if len(combo) < n*2:
+                        add_char(results, differential, opening_parens, closing_parens, combo, '(')
+                        add_char(results, differential, opening_parens, closing_parens, combo, ')')
+                    else:
+                        results.append(combo)
+                elif char == ')' and closing_parens > 0:
+                    combo += char
+                    closing_parens -= 1
+                    differential -= 1
+                    if len(combo) < n*2:
+                        add_char(results, differential, opening_parens, closing_parens, combo, '(')
+                        add_char(results, differential, opening_parens, closing_parens, combo, ')')
+                    else:
+                        results.append(combo)
 
-
-
-        def add_closing(results, differential, opening_parens, closing_parens, combo):
-            if closing_parens > 0 and differential >= 0:
-                combo += ')'
-                closing_parens -= 1
-                differential -= 1
-                if len(combo) < n*2:
-                    add_opening(results, differential, opening_parens, closing_parens, combo)
-                    add_closing(results, differential, opening_parens, closing_parens, combo)
-                else:
-                    results.append(combo)
-
-
-        add_opening(results, differential, opening_parens, closing_parens, combo)
-        add_closing(results, differential, opening_parens, closing_parens, combo)
+        add_char(results, differential, opening_parens, closing_parens, combo, '(')
+        add_char(results, differential, opening_parens, closing_parens, combo, ')')
 
         return results
